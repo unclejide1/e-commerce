0be2f00237ee301ef959163ef7e65169934855f2
Nwoke <nwoke@systemspecs.com.ng>
2021-02-22 16:54:10 +0100

    feat: add util function to format price

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,9 @@
-export const formatPrice = () => {}
+export const formatPrice = (number) => {
+  const newNumber = new Intl.NumberFormat('en-NG',{
+   style:'currency',
+   currency: 'NGN'
+  }).format(number/100)
+  return newNumber
+}
 
 export const getUniqueValues = () => {}
