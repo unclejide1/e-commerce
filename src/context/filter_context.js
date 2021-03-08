443e1578fa6f81d23e5b848d1f274bb3408fee3b
Nwoke <nwoke@systemspecs.com.ng>
2021-03-08 18:08:49 +0100

    feat: add sort functionality

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -15,7 +15,8 @@ import { useProductsContext } from './products_context'
 const initialState = {
   filtered_products: [],
   all_products: [],
-  grid_view: false
+  grid_view: false,
+  sort:'price-lowest'
 }
 
 const FilterContext = React.createContext()
@@ -36,9 +37,20 @@ export const FilterProvider = ({ children }) => {
   const setListView = () => {
     dispatch({ type: SET_LISTVIEW })
   }
+  const updateSort = (e) => {
+    // for demonstration
+    // e.preventDefault()
+    // const name = e.target.name;
+    const value = e.target.value;
+    dispatch({type:UPDATE_SORT, payload:value})
+  }
+
+  useEffect(() => {
+    dispatch({type:SORT_PRODUCTS})
+  }, [products, state.sort])
   
   return (
-    <FilterContext.Provider value={{...state, setGridView, setListView}}>
+    <FilterContext.Provider value={{...state, setGridView, setListView, updateSort}}>
       {children}
     </FilterContext.Provider>
   )
--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -27,6 +27,34 @@ const filter_reducer = (state, action) => {
         ...state,
         grid_view: false,
       }
+    case UPDATE_SORT:
+      return {
+        ...state,
+        sort: action.payload,
+      }
+    case SORT_PRODUCTS:
+      const { sort, filtered_products } = state
+      let tempProducts = [...filtered_products]
+      if (sort === 'price-lowest') {
+        tempProducts = tempProducts.sort((a, b) => a.price - b.price)
+      }
+      if (sort === 'price-highest') {
+        tempProducts = tempProducts.sort((a, b) => b.price - a.price)
+      }
+      if (sort === 'name-a') {
+        tempProducts = tempProducts.sort((a, b) => {
+          return a.name.localeCompare(b.name)
+        })
+      }
+      if (sort === 'name-z') {
+        tempProducts = tempProducts.sort((a, b) => {
+          return b.name.localeCompare(a.name)
+        })
+      }
+      return {
+        ...state,
+        filtered_products: tempProducts,
+      }
     default:
       break
   }
