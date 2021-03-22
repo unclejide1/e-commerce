4dddd6f66e4e26c8d0d4a826bd9c9406627796cc
Nwoke <nwoke@systemspecs.com.ng>
2021-03-22 16:57:44 +0100

    feat: set up cart context

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -6,6 +6,8 @@ import {
   TOGGLE_CART_ITEM_AMOUNT,
 } from '../actions'
 
+
+
 const cart_reducer = (state, action) => {
   return state
   throw new Error(`No Matching "${action.type}" - action type`)
