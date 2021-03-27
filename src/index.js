5324515ca7c7aed87209d201508c5c74f43f027f
Nwoke <nwoke@systemspecs.com.ng>
2021-03-27 02:14:28 +0100

    refactor: use .env file to get keys

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ import { Auth0Provider } from '@auth0/auth0-react'
 
 ReactDOM.render(
   <Auth0Provider
-    domain='dev-gyx4zmkb.us.auth0.com'
-    clientId='3x93fDskreDWQsM52T93rJNRjDxrylmi'
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_CLIENT_ID}
     redirectUri={window.location.origin}
     cacheLocation='localstorage'
   >
