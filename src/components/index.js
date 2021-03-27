ca5caa0195bded1cad804bee88a79c6808080809
Nwoke <nwoke@systemspecs.com.ng>
2021-03-27 02:16:49 +0100

    feat: add paystack integration to recieve payments

--- src/components/index.js
+++ src/components/index.js
@@ -17,6 +17,7 @@ import Stars from './Stars'
 import CartContent from './CarContent'
 import PageHero from './PageHero'
 import StripeCheckout from './StripeCheckout'
+import PaystackCheckout from './PaystackCheckout'
 export {
   FeaturedProducts,
   Navbar,
@@ -37,4 +38,5 @@ export {
   CartContent,
   PageHero,
   StripeCheckout,
+  PaystackCheckout
 }
